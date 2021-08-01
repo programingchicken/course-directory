2d0e8df786488f087b32874cf63c6000c7fd8169
programingchicken <aquacrazz.gp@gmail.com>
2021-08-01 16:44:21 -0700

    Updates

---
+++ precache-manifest.7c7a915572ae19229142b08ddcf5a518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b73dec317a61550c241",
+    "url": "/course-directory/static/css/main.d2df9868.chunk.css"
+  },
+  {
+    "revision": "6b73dec317a61550c241",
+    "url": "/course-directory/static/js/main.6b73dec3.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "751a1cf38c20392e6626029286b88dc2",
+    "url": "/course-directory/index.html"
+  }
+];
